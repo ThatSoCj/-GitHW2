6248325558d115484f555c3d0c2c7a4d4e8bda67
Christopher <coolscj123@gmail.com>
2026-09-08 17:04:33 -0400

    Add 2 dollar withdrawal fee

--- BankAccount.java
+++ BankAccount.java
@@ -9,11 +9,11 @@ public class BankAccount {
     // TODO: add the amount to the balance
     }
     public boolean withdraw(double amount) {
-    // TODO:
-    // Withdraw the requested amount if sufficient funds are available.
-    // Return true if the withdrawal succeeds.
-    // Return false otherwise. return false;
-    return false;
+        if (balance >= amount + 2) {
+            balance = balance - amount - 2;
+            return true;
+        }
+        return false;
     }
     public String getAccountSummary() {
     return accountHolder + ": $" + balance;
