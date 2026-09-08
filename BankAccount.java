4006c1bf2fa3936e4e71dae00f9d7e9ea26160c4
Christopher <coolscj123@gmail.com>
2026-09-08 17:02:40 -0400

    Require 50 dollar minimum balance

--- BankAccount.java
+++ BankAccount.java
@@ -9,11 +9,11 @@ public class BankAccount {
     // TODO: add the amount to the balance
     }
     public boolean withdraw(double amount) {
-    // TODO:
-    // Withdraw the requested amount if sufficient funds are available.
-    // Return true if the withdrawal succeeds.
-    // Return false otherwise. return false;
-    return false;
+        if (balance - amount >= 50) {
+            balance = balance - amount;
+            return true;
+        }
+        return false;
     }
     public String getAccountSummary() {
     return accountHolder + ": $" + balance;
